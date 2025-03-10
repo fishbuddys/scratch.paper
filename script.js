5c900fb2e08f8d162bd1d3b6fe490afd52af6b64
fishbuddys <fishbuddys@proton.me>
2025-03-10 06:47:37 +0000

    Yaay

---
+++ script.js
@@ -0,0 +1,36 @@
+
+var dragSound = document.getElementById('dragSound');
+
+// Preloads audio file
+document.addEventListener('DOMContentLoaded', function() {
+  dragSound.load();
+});
+
+function onMouseDown(event) {
+  dragSound.currentTime = 0;
+  dragSound.play();
+}
+
+function onMouseDrag(event) {
+  var baseSize = 6;
+  var size = baseSize + event.delta.length;
+  
+  var path = new Path.Rectangle({
+    center: event.point,
+    size: [size, size]
+  });
+  
+  var hue = (event.point.y / view.size.height) * 300;
+
+  path.fillColor = {
+    hue: hue,
+    saturation: 0.7,
+    brightness: 0.9
+  }
+}
+
+function onMouseUp(event) {
+
+  dragSound.pause();
+  dragSound.currentTime = 0;
+}
